6c989afdfd105c702f7c0e42b7723067dbfc5302
gmlrie001 <dev2@monzamedia.com>
2021-07-08 14:47:35 +0200

    Publishable models and requests added - update service provider to publish.

--- src/OrderCollectionServiceProvider.php
+++ src/OrderCollectionServiceProvider.php
@@ -9,6 +9,7 @@ class OrderCollectionServiceProvider extends ServiceProvider
 {  
     protected $defer = false;
 
+    
     /**
      * Bootstrap the application services.
      */
@@ -19,39 +20,19 @@ class OrderCollectionServiceProvider extends ServiceProvider
        * Optional methods to load your package assets
        * 
        */
-
       $this->loadMigrationsFrom( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'database/migrations');
+      $this->loadViewsFrom( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'resources/views/order_collection', 'order_collection' );
       $this->loadRoutesFrom( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'routes/web.php');
       // $this->loadRoutesFrom( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'routes/vault.php');
-
-      $this->loadViewsFrom( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'resources/views/order_collection', 'order_collection' );
       $this->publishes([
-        dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'config/order_collection.php' => 
-        config_path( 'order_collection.php' ),
-      ], 'config');
-
-      // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'order_collection');
-      // if ($this->app->runningInConsole()) {
-      //   $this->publishes([
-      //       __DIR__.'/../config/order_collection.php' => config_path( 'order_collection.php' ),
-      //     ], 'config');
-        // Publishing the views.
-        /*$this->publishes([
-          __DIR__.'/../resources/views' => resource_path( 'views/vendor/order_collection' ),
-        ], 'views' );*/
-        // Publishing assets.
-        /*$this->publishes([
-          __DIR__.'/../resources/assets' => public_path( 'vendor/order_collection' ),
-        ], 'assets' );*/
-        // Publishing the translation files.
-        /*$this->publishes([
-          __DIR__.'/../resources/lang' => resource_path( 'lang/vendor/order_collection' ),
-        ], 'lang' );*/
-        // Registering package commands.
-        // $this->commands([
-        //   Console\Commands\order_collection::class
-        // ]);
-      // }
+        dirname( __DIR__ ) .DIRECTORY_SEPARATOR. 'config/order_collection.php' => config_path( 'order_collection.php' ), 
+      ], 'order_collection_config');
+      // dirname( __DIR__ ) .DIRECTORY_SEPARATOR. 'config/order_collection.php' => config_path( 'order_collection.php' ), 
+      $this->publishes([
+        __DIR__ .DIRECTORY_SEPARATOR. 'Models' .DIRECTORY_SEPARATOR. 'Publish' .DIRECTORY_SEPARATOR. 'CollectionPoint.php' => app_path('Models' .DIRECTORY_SEPARATOR. 'CollectionPoint.php'), 
+        __DIR__ .DIRECTORY_SEPARATOR. 'Requests' .DIRECTORY_SEPARATOR. 'Publish' .DIRECTORY_SEPARATOR. 'CollectionPoint' .DIRECTORY_SEPARATOR. 'CreateRequest.php' => app_path('Http' .DIRECTORY_SEPARATOR. 'Requests' .DIRECTORY_SEPARATOR. 'CollectionPoint' .DIRECTORY_SEPARATOR. 'CreateRequest.php'), 
+        __DIR__ .DIRECTORY_SEPARATOR. 'Requests' .DIRECTORY_SEPARATOR. 'Publish' .DIRECTORY_SEPARATOR. 'CollectionPoint' .DIRECTORY_SEPARATOR. 'UpdateRequest.php' => app_path('Http' .DIRECTORY_SEPARATOR. 'Requests' .DIRECTORY_SEPARATOR. 'CollectionPoint' .DIRECTORY_SEPARATOR. 'UpdateRequest.php'), 
+      ], 'order_collection');
     }
 
     /**
@@ -60,17 +41,11 @@ class OrderCollectionServiceProvider extends ServiceProvider
     public function register()
     {
       // Automatically apply the package configuration
-      $this->mergeConfigFrom( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'config/order_collection.php', 'order_collection') ;
-
+      $this->mergeConfigFrom( dirname( __DIR__ ) .DIRECTORY_SEPARATOR. 'config/order_collection.php', 'order_collection') ;
       // Register the main class to use with the facade
-      $this->app->singleton( 'order_collection', function () {
-        return new OrderCollection();
-      });
-
+      $this->app->singleton('order_collection', function () { return new OrderCollection(); });
       // // Bind to main package class
-      // $this->app->bind( 'Portfolio', function () {
-     	//   return new Vault\Portfolio\Portfolio();
-      // });
+      // $this->app->bind('Portfolio', function () { return new Vault\Portfolio\Portfolio(); });
     }
 
     /**
@@ -80,10 +55,7 @@ class OrderCollectionServiceProvider extends ServiceProvider
      */
     public function provides()
     {
-      return [
-        // 'OrderCollection', 
-        'order_collection'
-      ];
+      return ['order_collection'/*, 'OrderCollection'*/];
     }
 
 }
