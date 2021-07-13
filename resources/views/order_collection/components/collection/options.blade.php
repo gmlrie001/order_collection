e00576934283bfd5a64df1fa3f2a07a8710a5c05
M. Riedaa Gamieldien <61379972+gmlrie001@users.noreply.github.com>
2021-07-13 16:29:30 +0200

    Update options.blade.php

--- resources/views/order_collection/components/collection/options.blade.php
+++ resources/views/order_collection/components/collection/options.blade.php
@@ -3,27 +3,27 @@
     @php
       $colOpts = $collection_points;
       $counter = 0;
-      $pointsOfCollection = $collection_points->groupBy( 'province' );
+      $pointsOfCollection = $collection_points->groupBy( 'province' )->so;
     @endphp
 
     @forelse($pointsOfCollection as $key=>$collection_points )
-      <details class="row regional-collection-points" @if( $counter === 0 ) open @endif>
-        <summary class="px-lg-3 px-0">{{ ucwords( $key ) }}</summary>
+      <details class="row regional-collection-points mb-lg-3 mb-4" @if( $counter === 0 ) open @endif>
+        <summary class="p-lg-3 p-0">{{ ucwords( $key ) }}</summary>
         @foreach( $collection_points as $k=>$option)
 
           @isset( $option )
           <div class="content col-12 local-collection-points mb-lg-3 mb-2" style="border-bottom:1px solid #f0f0f0;">
-            <div class="row my-lg-2 my-3">
+            <div class="row no-gutters my-lg-2 my-3">
 
-              <div class="col-auto col-lg-1 mt-1 mt-lg-0 pr-0 pr-lg-3">
+              <div class="col-auto col-lg-1 mt-1 mt-lg-0 text-center">
                 @php
                   $address  = ( isset( $option->address_line_1 ) ) ? $option->address_line_1.', ' : ''; 
                   $address .= ( isset( $option->address_line_2 ) ) ? $option->address_line_2.', ' : '';
-                  $address .= ( isset( $option->suburb ) ) ? $option->suburb.', ' : '';
-                  $address .= ( isset( $option->city ) ) ? $option->city.', ' : ''; 
-                  $address .= ( isset( $option->province ) ) ? $option->province.', ' : '';
-                  $address .= ( isset( $option->country ) ) ? $option->country.', ' : '';
-                  $address .= ( isset( $option->postal_code ) ) ? $option->postal_code : '';
+                  $address .= ( isset( $option->suburb ) )         ? $option->suburb.', '         : '';
+                  $address .= ( isset( $option->city ) )           ? $option->city.', '           : ''; 
+                  $address .= ( isset( $option->province ) )       ? $option->province.', '       : '';
+                  $address .= ( isset( $option->country ) )        ? $option->country.', '        : '';
+                  $address .= ( isset( $option->postal_code ) )    ? $option->postal_code         : '';
                 @endphp
 
                 <input id="option_{{ $counter }}"
@@ -40,7 +40,7 @@
                 <label for="option_{{ $counter }}"></label>
               </div>
 
-              <div class="col col-lg-2 strong mt-1 mt-lg-0 pl-lg-3 pl-3">
+              <div class="col col-lg-2 strong mt-1 mt-lg-0">
                 {{ $option->shipping_title ?? $option->title }}
               </div>
               <div class="col-12 col-lg-4 mt-1 mt-lg-0">
@@ -49,8 +49,8 @@
               <div class="col-12 col-lg-4 mt-1 mt-lg-0">
                 {{ $option->shipping_time }}
               </div>
-              <div class="col-12 col-lg-1 mt-1 mt-lg-0">R
-                {{ number_format( $option->shipping_cost, 2, ".", "" ) }}
+              <div class="col-12 col-lg-1 mt-1 mt-lg-0">
+                R {{ number_format( $option->shipping_cost, 2, ".", "" ) }}
               </div>
 
               <div class="col-12 offset-0 col-lg-11 offset-lg-1">
@@ -95,7 +95,7 @@
                     @isset( $impossible_variable )
                     <div class="col-12 @isset( $impossible_variable ) col-lg-7 @else col-lg-12 @endisset mt-3 mt-lg-0">
                       @isset( $option->id )
-                        <div style="width:100%;height:250px;" class="decollect-maps" id="map_{{ $option->id }}"></div>
+                        <div style="width:100%;height:250px;" class="collect-maps" id="map_{{ $option->id }}"></div>
                       @endisset
                     </div>
                     @endisset
