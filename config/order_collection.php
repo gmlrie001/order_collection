86ccfadb26d3061132ecb4d76a149883797fbe2f
M. Riedaa Gamieldien <61379972+gmlrie001@users.noreply.github.com>
2022-02-10 08:48:32 +0200

    Update order_collection.php

--- config/order_collection.php
+++ config/order_collection.php
@@ -1,13 +1,12 @@
 <?php
 
-/**
- * Portfolio module config settings.
- *
- */
 return [
+  /**
+   * Order Collection module config settings.
+   *
+   */
 
   'namespace' => 'Vault\\OrderCollection',
-
 
   /**
    * SEEDERS for the CollectionPoint model
@@ -18,34 +17,31 @@ return [
       'shipping_title'       => 'Option to be selected from the list.',
       'shipping_description' => 'Processsing - preparing options for presentation.',
 
-      'address_line_1'       => 'Unit 105, Platinum Junction Business Park',
-      'address_line_2'       => 'School Street',
-      'postal_code'          => '7441',
-      'suburb'               => 'Milnerton',
+      'address_line_1'       => '',
+      'address_line_2'       => '',
+      'postal_code'          => '',
+      'suburb'               => '',
       'province'             => 'Western Cape',
       'city'                 => 'Cape Town',
       'country'              => 'South Africa',
 
-      'latitude'             => '-33.8770001',
-      'longitude'            => '18.5033585',
-
-      'shipping_time'        => null,
+      'shipping_time'        => NULL,
       'collection_code'      => 'PROCESSING',
-      'trading_hours'        => null, 
-      'shipping_cost'        => 0.00,
+      'trading_hours'        => NULL, 
+      'shipping_cost'        => number_format( 0, 2, ".", "" ),
 
-      'created_at'           => '2020-04-07 10:10:43',
-      'updated_at'           => '2020-06-27 11:56:12',
-      'deleted_at'           => null,
+      'created_at'           => date( 'Y-m-d H:i:s', strtotime("now") ),
+      'updated_at'           => date( 'Y-m-d H:i:s', strtotime("now") ),
+      'deleted_at'           => NULL,
 
       'status'               => 'DRAFT',
-      'status_date'          => null,
+      'status_date'          => NULL,
 
       'order'                => 1,
     ], [
       'title'                => 'Collect from us',
       'shipping_title'       => 'Collect from us',
-      'shipping_description' => 'Collection From All American Auto Store',
+      'shipping_description' => 'Collection From Milnerton High School Pool',
 
       'address_line_1'       => 'Unit 105, Platinum Junction Business Park',
       'address_line_2'       => 'School Street',
@@ -55,20 +51,17 @@ return [
       'city'                 => 'Cape Town',
       'country'              => 'South Africa',
 
-      'latitude'             => '-33.8770001',
-      'longitude'            => '18.5033585',
-
-      'shipping_time'        => '1-3 Days Upon Arrival in Port',
-      'collection_code'      => 'AA_COLLECT',
+      'shipping_time'        => '2-3 days after order has been confirmed',
+      'collection_code'      => 'IS_COLLECT',
       'trading_hours'        => "<p>Mon-Fri: 08:00 - 17:00</p><p>Sat: 09:00 - 13:00</p><p>Sun: Closed</p>",
-      'shipping_cost'        => 0.00,
+      'shipping_cost'        => number_format( 0, 2, ".", "" ),
 
-      'created_at'           => '2020-04-07 10:20:43',
-      'updated_at'           => '2020-06-27 12:06:12',
-      'deleted_at'           => null,
+      'created_at'           => date( 'Y-m-d H:i:s', strtotime("now") ),
+      'updated_at'           => date( 'Y-m-d H:i:s', strtotime("now") ),
+      'deleted_at'           => NULL,
 
       'status'               => 'PUBLISHED',
-      'status_date'          => null,
+      'status_date'          => NULL,
 
       'order'                => 2,
     ], 
