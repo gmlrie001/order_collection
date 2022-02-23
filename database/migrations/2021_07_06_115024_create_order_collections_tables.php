2f071e325d78a690d0a9b9651ad0b87e6874bf61
M. Riedaa Gamieldien <61379972+gmlrie001@users.noreply.github.com>
2022-02-23 20:52:11 +0200

    Update 2021_07_06_115024_create_order_collections_tables.php

--- database/migrations/2021_07_06_115024_create_order_collections_tables.php
+++ database/migrations/2021_07_06_115024_create_order_collections_tables.php
@@ -13,70 +13,60 @@ class CreateOrderCollectionsTables extends Migration
    */
   public function up()
   {
-    if ( ! Schema::hasTable( 'collection_points' ) ) {
-      Schema::create( 'collection_points', function ( Blueprint $table ) {
-        $table->increments( 'id' );
+    if (! Schema::hasTable('collection_points')) {
+      Schema::create('collection_points', function (Blueprint $table) {
+        $table->increments('id');
 
-        $table->string( 'title' );
-        $table->string( 'shipping_title' )->nullable();
-        $table->longText( 'shipping_description' )->nullable();
+        $table->string('title' );
+        $table->string('shipping_title' )->nullable();
+        $table->longText('shipping_description' )->nullable();
 
-        $table->text( 'address_line_1' )->nullable();
-        $table->text( 'address_line_2' )->nullable();
-        $table->text( 'postal_code' )->nullable();
-        $table->text( 'suburb' )->nullable();
-        $table->text( 'province' )->nullable();
-        $table->text( 'city' )->nullable();
-        $table->text( 'country' )->nullable();
+        $table->text('address_line_1')->nullable();
+        $table->text('address_line_2')->nullable();
+        $table->text('postal_code')->nullable();
+        $table->text('suburb')->nullable();
+        $table->text('province')->nullable();
+        $table->text('city')->nullable();
+        $table->text('country')->nullable();
 
-        $table->text( 'latitude' )->nullable();
-        $table->text( 'longitude' )->nullable();
+        $table->text('latitude')->nullable();
+        $table->text('longitude')->nullable();
 
-        $table->text( 'shipping_time' )->nullable();
-        $table->string( 'collection_code' )->nullable();
-        $table->longText( 'trading_hours' )->nullable();
-        $table->decimal( 'shipping_cost', 10, 2 )
+        $table->text('shipping_time')->nullable();
+        $table->string('collection_code')->nullable();
+        $table->longText('trading_hours')->nullable();
+        $table->decimal('shipping_cost', 10, 2)
               ->default(0.00)->nullable();
-
-        // $table->bigInteger( 'basket_id' )->unSigined()->nullable();
 
         $table->timestamps();
         $table->softDeletes();
 
-        $table->enum( 'status', ['PUBLISHED','UNPUBLISHED','SCHEDULED','DRAFT'] )
+        $table->enum('status', ['PUBLISHED','UNPUBLISHED','SCHEDULED','DRAFT'])
               ->default('PUBLISHED')->nullable();
-        $table->dateTime( 'status_date' )->nullable();
+        $table->dateTime('status_date')->nullable();
 
         $table->bigInteger('order')->default(1)->nullable();
       });
     }
 
     /** Seeding table */
-    $insertionArray = [];
-    $n = count( config( 'order_collection.collection_point_seeder' ) ) - 1;
+    $insertionArray = config('order_collection.collection_point_seeder');
+    \DB::beginTransaction();
 
-    foreach( range( 0, $n ) as $i ) {
-      \DB::table( 'collection_points' )->insert([
-        $i => config( 'order_collection.collection_point_seeder' )[$i], 
-      ]);
-      // $insertionArray[$i] = config( 'order_collection.collection_point_seeder' )[$i];
-    }
-
-  /*
-    // \DB::beginTransaction();
     try {
-      // \DB::table( 'collection_points' )->insert( $insertionArray );
-      // \DB::commit();
-      ( new \App\Models\CollectionPoint )->insert( $insertionArray );
-      dump( ( new \App\Models\CollectionPoint )->get() );
+      \DB::table('collection_points')
+         ->insert($insertionArray);
 
-    } catch ( \Exception $error ) {
-      info( $error->getMessage() );
-      // \DB::rollBack();
+      \DB::commit();
+
+    } catch (\Exception $error) {
+      info([
+        $error->getMessage(), 
+        $error->getCode()
+      ]);
+
+      \DB::rollback();
     }
-    // \DB::commit();
-    // dd( $insertionArray, ( new \App\Models\CollectionPoint ), \DB::table( 'collection_points' )->select( '*' ) );
-  */
   }
 
   /**
@@ -86,10 +76,8 @@ class CreateOrderCollectionsTables extends Migration
    */
   public function down()
   {
-    if ( Schema::hasTable( 'collection_points' ) ) {
-      // \DB::table( 'collection_points' )->truncate();
-
-      Schema::dropIfExists( 'collection_points' );
+    if (Schema::hasTable('collection_points')) {
+      Schema::dropIfExists('collection_points');
     }
   }
 
