d58b3c59edcd63e1ff231406c031271c1b4b35df
M. Riedaa Gamieldien <61379972+gmlrie001@users.noreply.github.com>
2022-02-23 20:58:29 +0200

    Update CollectionPointController.php (#6)

--- src/Http/Controllers/Page/CollectionPointController.php
+++ src/Http/Controllers/Page/CollectionPointController.php
@@ -3,33 +3,26 @@
 namespace Vault\OrderCollection\Http\Controllers\Page;
 
 use App\Http\Controllers\Services\PageController;
-
 use Illuminate\Http\Request;
-
 
 use Vault\OrderCollection\OrderCollection;
 
 
 class CollectionPointController extends PageController
 {
-
   public $collect;
-
 
   public function __construct()
   {
     parent::__construct();
-
     $this->collect = new OrderCollection();
 
     return $this;
   }
 
-  public function index( Request $request )
+  public function index(Request $request)
   {
-    // dd( __METHOD__, __LINE__, $this, new \ReflectionClass( $this->collect ), request()->all() );
-
-    return $this->collect->collection( $request );
+    return $this->collect->collection($request);
   }
 
 }
